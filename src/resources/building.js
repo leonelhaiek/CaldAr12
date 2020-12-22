5bef5f3b8c36d19d5efb800665b5abd467fff23b
leonelhaiek <leonelhaiek@outlook.com>
2020-12-22 16:10:33 -0300

    validation fix

--- src/resources/building.js
+++ src/resources/building.js
@@ -27,7 +27,7 @@ const buildingRes =
         {id:'phone',type: 'number', name:'Phone Number', onError:'Must have at least 8 digits', pattern: /^[0-9]{8,}$/},
         {id:'address',type: 'text', name:'Address', onError:'Must have at least 5 characters with numbers and letters', pattern: /^([a-z0-9]{2,}[\s]+)+([0-9]+)$/i},
         {id:'boiler',type: 'text', name:'Boilers', onError:'Must have at least 1 characters', pattern: /^[a-z;,]{1,}$/i},
-        {id:'capabilities',type: 'text', name:'Capabilities', onError:'Must have at least 3 characters', pattern: /^[a-z]{1,}$/i},
+        {id:'capabilities',type: 'number', name:'Capabilities', onError:'Must have at least 3 characters', pattern: /^[a-z0-9]{1,}$/i},
         {id:'hour_rate',type: 'number', name:'Hour Rate', onError:'Must have at least 1 digits', pattern: /^[0-9]{1,}$/},
         {id:'daily_capacity',type: 'number', name:'Daily Capacity', onError:'Must have at least 1 digits', pattern: /^[0-9]{1,}$/},
       ]
